c7a66903ad359c687c19193d2bba9afa8fbc0da8
ravidro1 <ravidrrt@gmail.com>
2023-02-15 15:55:51 +0200

    addtion: Nav-Bar

--- src/Components/AddTabel.jsx
+++ src/Components/AddTabel.jsx
@@ -1,76 +1,62 @@
-import React, { useContext, useEffect, useRef, useState } from 'react';
-import { Context } from '../App';
-import DefineDataTypes from './DefineDataTypes';
+import React, {useContext, useEffect, useRef, useState} from "react";
+import {Context} from "../App";
+import DefineDataTypes from "./DefineDataTypes";
+
+import axios from "axios";
 
 function AddTabel(props) {
-    const {dataTypes, typeOfFieldsObj, setTypeOfFieldsObj} = useContext(Context);
+  const {dataTypes, typeOfFieldsObj, setTypeOfFieldsObj} = useContext(Context);
 
-    const [tabelArray, setArray] = useState([
-      [
-        "Full Name",
-        "Job Title",
-        "Department",
-        "Business Unit",
-        "Gender",
-        "Ethnicity",
-        "Age",
-      ],
-    ]);
-  
-    const fileInputRef = useRef();
-  
-    const [tempFile, setTempFile] = useState();
-    const [Tabel, setTabel] = useState();
-  
-    const addTabel = () => {
-      setTempFile("");
-  
+  const [tabelArray, setArray] = useState([
+    [
+      "Full Name",
+      "Job Title",
+      "Department",
+      "Business Unit",
+      "Gender",
+      "Ethnicity",
+      "Age",
+    ],
+  ]);
 
-    //   axios
-    //     .post("")
-    //     .then((res) => {
-    //         setTabel(res.data.tabel);
-    //         fileInputRef.current.value = [];
-    //     })
-    //     .catch((err) => {
-    //       console.log(err);
-    //     });
-  
-    };
-  
-    const [isAllTheFieldsWithDataType, setIsAllTheFieldsWithDataType] =
-      useState(false);
-  
-    useEffect(() => {
-      if (typeOfFieldsObj) {
-        let isFill = true;
-  
-        Object.keys(typeOfFieldsObj).forEach((key) => {
-          if (typeOfFieldsObj[key] == "") isFill = false;
-        });
-  
-        setIsAllTheFieldsWithDataType(isFill);
-      }
-    }, [typeOfFieldsObj]);
-  
+  const fileInputRef = useRef();
 
-    
+  const [tempFile, setTempFile] = useState();
+  const [Tabel, setTabel] = useState();
 
-    return (
-      <div>
-        <input
-          ref={fileInputRef}
-          onChange={(e) => {
-            setTempFile(e.target.files[0]);
-          }}
-          type={"file"}
-        />
-        <button onClick={addTabel}> Add Tabel </button>
-  
-        {Tabel && <DefineDataTypes tabelArray={tabelArray} />}
-        {isAllTheFieldsWithDataType && <button> submit </button>}
-      </div>
-    );
-  }
+
+
+  const [isAllTheFieldsWithDataType, setIsAllTheFieldsWithDataType] =
+    useState(false);
+
+  useEffect(() => {
+    if (typeOfFieldsObj) {
+      let isFill = true;
+
+      Object.keys(typeOfFieldsObj).forEach((key) => {
+        if (typeOfFieldsObj[key] == "") isFill = false;
+      });
+
+      setIsAllTheFieldsWithDataType(isFill);
+    }
+  }, [typeOfFieldsObj]);
+
+
+  return (
+    <div>
+      <input
+        ref={fileInputRef}
+        onChange={(e) => {
+          setTempFile(e.target.files[0]);
+        }}
+        type={"file"}
+      />
+      <button> Add Tabel </button>
+
+      {Tabel && <DefineDataTypes tabelArray={tabelArray} />}
+      {isAllTheFieldsWithDataType && <button> submit </button>}
+    </div>
+  );
+}
 
 export default AddTabel;
