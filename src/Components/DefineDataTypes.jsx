5e893b48feb2e29955e4aaeebe3c43fb0def810a
ravidro1 <ravidrrt@gmail.com>
2023-02-15 23:40:01 +0200

    commit for pull

--- src/Components/DefineDataTypes.jsx
+++ src/Components/DefineDataTypes.jsx
@@ -1,56 +1,30 @@
-import React, { useState } from "react";
-import { useEffect } from "react";
-import { useContext } from "react";
-import { Context } from "../App";
+import React, {useState} from "react";
+import {useEffect} from "react";
+import {useContext} from "react";
+import {Context} from "../App";
 import DataContext from "../DataContext";
 import AddTabel from "./AddTabel";
 
-function DefineDataTypes({ tabelArray }) {
-  const { dataTypes, typeOfFieldsObj, setTypeOfFieldsObj } = useContext(Context);
+function DefineDataTypes({tabelArray}) {
+  const {dataTypes, typeOfFieldsObj, setTypeOfFieldsObj} = useContext(Context);
 
   useEffect(() => {
-    setTypeOfFieldsObj(() => {
-      const tempObj = {};
-
-      tabelArray && Object.keys(tabelArray[0])?.forEach((key, index) => {
-
-        tempObj[key] = "";
-
-      })
-
-
-      return tempObj;
-    });
-  }, []);
-
-
-  let t = [
-    [
-      "Full Name",
-      "Job Title",
-      "Department",
-      "Business Unit",
-      "Gender",
-      "Ethnicity",
-      "Age",
-    ],
-  ]
+    if (typeOfFieldsObj) {
+    }
+  }, [typeOfFieldsObj]);
 
   const changeTypeOfFieldsObj = (dataKey, newType) => {
-    const newTypeOfFieldsObj = { ...typeOfFieldsObj, [dataKey]: newType };
+    const newTypeOfFieldsObj = {...typeOfFieldsObj, [dataKey]: newType};
     setTypeOfFieldsObj(newTypeOfFieldsObj);
   };
 
   return (
     <div>
       <div>
-        {tabelArray && Object.keys(tabelArray[0])?.map((key, index) => {
+        {Object.keys(typeOfFieldsObj)?.map((key, index) => {
           return (
             <div key={index}>
-              <input
-                disabled
-                defaultValue={[key].toString().trim()}
-              />
+              <input disabled defaultValue={[key].toString().trim()} />
               <select
                 onChange={(e) => changeTypeOfFieldsObj(key, e.target.value)}
                 name=""
